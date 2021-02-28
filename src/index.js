8b98a74ad2178a07c5f6e07b5ebf2e35153be179
kykysja <kykysja11@gmail.com>
2021-02-28 19:59:33 +0100

    feat: implement the function of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+	if (array) {
+		let m = 0;
+		for (i = 0; i < array.length; i++) {
+			if (array[i] < m) {
+				m = array[i];
+			}
+		}
+		return m;
+	} else {
+		return 0;
+	}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+	if (array) {
+		let m = 0;
+		for (i = 0; i < array.length; i++) {
+			if (array[i] > m) {
+				m = array[i];
+			}
+		}
+		return m;
+	} else {
+		return 0;
+	}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+	if (array) {
+		let sum = 0;
+		for (i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		if (sum == 0) {
+			return 0;
+		} else {
+			return sum / array.length;
+		}
+	} else {
+		return 0;
+	}
 }
